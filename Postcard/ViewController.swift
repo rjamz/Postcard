1e45dc7840d0d99dd0c4fbf3ea8c38e94d6d9698
Richard James <rajames@Richards-MacBook-Pro.local>
2014-10-23 23:54:09 -0400

    added name label functionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
     
+    @IBOutlet weak var nameLabel: UILabel!
 
     
     override func viewDidLoad() {
@@ -35,6 +36,13 @@ class ViewController: UIViewController {
             
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        nameLabel.resignFirstResponder()
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
